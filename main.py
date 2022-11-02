54413f9b5d093d153a1a55c3b559909ef3f1371b
PixelWelt <{ID}+{username}@users.noreply.github.com>
2022-11-02 19:31:10 +0100

    added function to implement dry principle

--- main.py
+++ main.py
@@ -9,42 +9,49 @@ username = creds.username
 
 driver = webdriver.Chrome(creds.webdriverPath)
 
-driver.get("https://portal.rama-mainz.de")
-
 usernameElem = driver.find_element(By.ID, "username")
 passwordElem = driver.find_element(By.ID, "password")
 
-#Login
-usernameElem.clear()
-usernameElem.send_keys(username)
-passwordElem.clear()
-passwordElem.send_keys(password)
 
-driver.find_element(By.ID, "bttSenden").click()
+# Login
+def Login(username, password):
+    driver.get("https://portal.rama-mainz.de")
+    usernameElem.clear()
+    usernameElem.send_keys(username)
+    passwordElem.clear()
+    passwordElem.send_keys(password)
+    driver.find_element(By.ID, "bttSenden").click()
 
-driver.get("https://portal.rama-mainz.de/stdplan.php")
 
-stdplan = driver.find_element(By.CLASS_NAME, "content")
+def GetTimeTable():
+    Login(creds.username, creds.password)
+    driver.get("https://portal.rama-mainz.de/stdplan.php")
+    stdplan = driver.find_element(By.CLASS_NAME, "content")
+    return stdplan
+
+
+def GetVertretung():
+    Login(creds.username, creds.password)
+    driver.get("https://portal.rama-mainz.de/vertretung.php")
+    dates = driver.find_elements(By.CLASS_NAME, "tagesbutton")
+    infosOfTheDays = driver.find_elements(By.CLASS_NAME, "nzts")
+    return dates, infosOfTheDays
+
+
+dates, infosOfTheDays = GetVertretung()
 
 with open('stundenplan.html', 'w') as f:
-    f.write(stdplan.get_attribute("innerHTML"))
+    f.write(GetTimeTable().get_attribute("innerHTML"))
+    if (len(dates) != 0):
+        with open('infosOfTheDay.txt', 'w') as f:
+            f.writelines(dates[0].text + "\n")
+            f.write(infosOfTheDays[0].text)
 
-driver.get("https://portal.rama-mainz.de/vertretung.php")
-
-dates = driver.find_elements(By.CLASS_NAME, "tagesbutton")
-print(len(dates))
-infosOfTheDays = driver.find_elements(By.CLASS_NAME, "nzts")
-if(len(dates) != 0):
-    with open('infosOfTheDay.txt', 'w') as f:
-        f.writelines(dates[0].text + "\n")
-        f.write(infosOfTheDays[0].text)
-
-
-    dates[1].click()
-    with open('infosOfTomorrow.txt', 'w') as f:
-        f.writelines(dates[1].text + "\n")
-        f.write(infosOfTheDays[1].text)
+        dates[1].click()
+        with open('infosOfTomorrow.txt', 'w') as f:
+            f.writelines(dates[1].text + "\n")
+            f.write(infosOfTheDays[1].text)
+    else:
+        print("Portal currently broken")
 
 driver.quit()
-
- 
