7bb708b07ffed96f8edb77c597214592d0278b26
PixelWelt <{ID}+{username}@users.noreply.github.com>
2022-11-05 22:41:04 +0100

    finished file Processing

--- processData.py
+++ processData.py
@@ -8,14 +8,45 @@ def convertDate(line):
     print(date)
     return date
 
+def convertTeachers(line):
+    teacherScore = list()
+    line = line.replace("\n", "")
+    line = line.replace(",", "")
+    itemList = line.split(" ")
+    teacherList = itemList[2:]
+    for teacher in teacherList:
+        if teacher.find("(") != -1:
+            indexOfTeacher = teacherList.index(teacher)
+            teacher = teacher.split("(")[1]
+            teacher = teacher.replace(")", "")
+            score = (eval(teacher) * -0.1 ) + 0.1
+            teacherScore.append(score)
+            teacherList[indexOfTeacher] = teacherList[indexOfTeacher].replace("(" + str(teacher) + ")", "")
+        else:
+            teacherScore.append(1)
+    print(line)
+    return teacherList, teacherScore
+
+def pushIntoDatabase(date, teacherList, teacherScore):
+    print()
+    #TODO: add server connection
+
+
 line = ""
 with open("infosOfTheDay.txt") as f:
-    for line in f:
-        print(line)
-
-
+    line = f.readline()
+    if "" != line:
+        date = convertDate(line)
+    line = f.readline()
+    if "" != line:
+        teacherList, teacherScore = convertTeachers(line)
+print(teacherList, teacherScore)
 with open("infosOfTomorrow.txt") as f:
     line = f.readline()
     if "" != line:
-        date = convertDate()
+        date = convertDate(line)
     line = f.readline()
+    if "" != line:
+        teacherList, teacherScore = convertTeachers(line)
+print(teacherList, teacherScore)
+
